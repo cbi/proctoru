af61db05145ba78c5946b8ac267f4c06b8c3b9e9
Joe Wright <joe.wright@noventech.com>
2014-05-09 11:38:29 -0500

    Move accessors to configuration file.

--- lib/proctoru/configuration.rb
+++ lib/proctoru/configuration.rb
@@ -1,9 +1,24 @@
-class Configuration
-  attr_accessor :api_key
-  attr_accessor :api_url
+module Proctoru
+  class Configuration
+    attr_accessor :api_key
+    attr_accessor :api_url
 
-  def initialize
-    @api_key = ""
-    @api_url = "https://apitest.proctoru.com/"
+    def initialize
+      @api_key = ""
+      @api_url = "https://apitest.proctoru.com/"
+    end
   end
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
 end
